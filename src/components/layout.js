c841fc880f6e2b55fab2309118cd8438a039b097
Eric Frommelt <ericfrommelt@gmail.com>
2021-08-02 11:57:30 -0400

    update font link

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,7 @@ const Layout = ({ children }) => {
       <Helmet>
         <html lang="en" />
         <title>Vaporized Claims</title>
-        <link rel="stylesheet" href="https://use.typekit.net/cni2rpi.css"></link>
-        <link rel="stylesheet" href="https://cloud.typography.com/7804816/6709192/css/fonts.css" />
+        <link rel="stylesheet" href="https://cloud.typography.com/7804816/7505032/css/fonts.css" />
       </Helmet>
       <main>
         {children}
