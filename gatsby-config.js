6001eaa86a4d0b5c5a7f659661358570c06bb668
Eric Frommelt <ericfrommelt@gmail.com>
2021-08-02 12:06:26 -0400

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
         options: {
             // You can add multiple tracking ids and a pageview event will be fired for all of them.
             trackingIds: [
-            "G-62LWH56VMD",
+            "G-5NNPBTJEN2",
             ],
         },
           },
